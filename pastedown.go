b6a75633f940eefcf91005b00cd57f7cf70dde77
Caleb Spare <cespare@gmail.com>
2012-11-23 01:41:39 -0800

    Get initial markdown rendering with pygments working.

--- pastedown.go
+++ pastedown.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	"bytes"
 	"github.com/cespare/blackfriday"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 )
+
+var (
+	pygmentize         = "./vendor/pygments/pygmentize"
+	validLanguages     = make(map[string]struct{})
+	markdownRenderer   *blackfriday.Html
+	markdownExtensions int
+)
+
+func init() {
+	// Get the list of valid lexers from pygments.
+	rawLexerList, err := exec.Command(pygmentize, "-L", "lexers").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, line := range bytes.Split(rawLexerList, []byte("\n")) {
+		if len(line) == 0 || line[0] != '*' {
+			continue
+		}
+		for _, l := range bytes.Split(bytes.Trim(line, "* :"), []byte(",")) {
+			lexer := string(bytes.TrimSpace(l))
+			if len(lexer) != 0 {
+				validLanguages[lexer] = struct{}{}
+			}
+		}
+	}
+
+	// Set up the renderer.
+	flags := 0
+	flags |= blackfriday.HTML_GITHUB_BLOCKCODE
+	markdownRenderer = blackfriday.HtmlRenderer(flags, "", "")
+	markdownRenderer.SetBlockCodeProcessor(syntaxHighlight)
+
+	markdownExtensions = 0
+	markdownExtensions |= blackfriday.EXTENSION_FENCED_CODE
+	markdownExtensions |= blackfriday.EXTENSION_TABLES
+	markdownExtensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	markdownExtensions |= blackfriday.EXTENSION_SPACE_HEADERS
+}
+
+func syntaxHighlight(out io.Writer, in io.Reader, language string) {
+	_, ok := validLanguages[language]
+	if !ok || language == "" {
+		language = "text"
+	}
+	pygmentsCommand := exec.Command(pygmentize, "-l", language, "-f", "html")
+	pygmentsCommand.Stdin = in
+	pygmentsCommand.Stdout = out
+	pygmentsCommand.Run()
+}
 
 func main() {
 	contents, err := ioutil.ReadFile("test.md")
 	if err != nil {
 		panic(err)
 	}
-	flags := 0
-	flags |= blackfriday.HTML_COMPLETE_PAGE
-	flags |= blackfriday.HTML_GITHUB_BLOCKCODE
-	renderer := blackfriday.HtmlRenderer(flags, "Title!", "")
-
-	extensions := 0
-	extensions |= blackfriday.EXTENSION_FENCED_CODE
-	extensions |= blackfriday.EXTENSION_TABLES
-	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
-	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
-	output := blackfriday.Markdown(contents, renderer, extensions)
+	output := blackfriday.Markdown(contents, markdownRenderer, markdownExtensions)
 	os.Stdout.Write(output)
 }
